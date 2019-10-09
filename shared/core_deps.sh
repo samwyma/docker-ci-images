6185fe3d10732a31dcb98ecb6b1f140b36c3b770
Samuel Wyma <sam@landtech.co>
2019-10-09 17:16:50 +0100

    Fix ordering of package install such that we get the latest version of docker-compose

--- shared/core_deps.sh
+++ shared/core_deps.sh
@@ -31,19 +31,19 @@ apk add --no-cache \
   make \
   python3-dev
 
-# pip
-pip3 install --upgrade \
-  pip \
-  pipenv \
-  docker-compose \
-  credstash
-
 # aws cli
 pip3 install --upgrade awscli
 mkdir -p /root/.aws/cli
 curl -s -o /root/.aws/cli/alias https://raw.githubusercontent.com/landtechnologies/reformation/master/assets/aws-alias
 
 pip3 install awsebcli==3.14.7
+
+# pip
+pip3 install --upgrade \
+  pip \
+  pipenv \
+  docker-compose \ # This must be installed after awsebcli==3.14.7 as awsebcli v3.14.7 has requirements for docker-compose >=1.21.2,<1.22.0
+  credstash
 
 # bats
 git clone https://github.com/sstephenson/bats.git
